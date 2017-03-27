b181f6765ae6b3eaaa59d1360381cbe31f33ac5e
naveenkothiyal <naveen.kothiyal@tothenew.com>
2017-03-27 14:27:54 +0530

    added logger framework

---
+++ Logger.framework/Headers/Logger.h
@@ -0,0 +1,19 @@
+//
+//  Logger.h
+//  Logger
+//
+//  Created by Naveen on 15/03/17.
+//  Copyright © 2017 TTN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Logger.
+FOUNDATION_EXPORT double LoggerVersionNumber;
+
+//! Project version string for Logger.
+FOUNDATION_EXPORT const unsigned char LoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logger/PublicHeader.h>
+
+
